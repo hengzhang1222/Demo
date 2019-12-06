b68d5166185bb8a8f0fa90f243dcab6f4d2e9557
hengzhang1222 <hengzhang1222@163.com>
2019-12-06 22:55:08 +0800

    teacherShenDemo

---
+++ src/test/java/com/zeroten/flow/IfElseTest.java
@@ -0,0 +1,16 @@
+package com.zeroten.flow;
+
+
+import org.junit.Test;
+
+public class IfElseTest {
+
+  /*  public static void main(String[] args) {
+        System.out.println("aaa");
+    }
+*/
+    @Test
+    public void test(){
+      System.out.println("aaa");
+    }
+}
